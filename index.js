63b0478be591f9318d93b789404811c48db95a21
sadafshahab12 <sadafshahab123@yahoo.com>
2025-06-28 17:12:31 -0700

    done

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 import express from "express";
 import { tableRouter } from "./routes/tableRouter.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
 
+// This is needed for __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.set("view engine", "ejs");
-app.set("views", "views");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(tableRouter);
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+
+// For Vercel, no app.listen()
+export default app;
